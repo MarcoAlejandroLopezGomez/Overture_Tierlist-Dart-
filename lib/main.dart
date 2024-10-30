ba5124023bbdf18e2ebaabdb87a12f5f3bce4676
MarcoAlejandroLopezGomez <A01572601@tec.mx>
2024-10-30 17:21:14 -0600

    Add red_cross.png asset to pubspec.yaml still doesn't work the cross out mode correctly

--- lib/main.dart
+++ lib/main.dart
@@ -50,6 +50,12 @@ class TierListPageState extends State<TierListPage> {
     );
   }
 
+  void _toggleCrossOut(ImageData imageData) {
+    setState(() {
+      imageData.crossedOut = !imageData.crossedOut;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -70,60 +76,64 @@ class TierListPageState extends State<TierListPage> {
   }
 
   Widget buildPickRows() {
-  return Container(
-    height: MediaQuery.of(context).size.height * 0.6, // Adjust the height as needed
-    child: Column(
-      children: customers.map((customer) {
-        return Expanded(
-          child: CustomerCart(
-            customer: customer,
-            highlighted: false,
-            onImageDropped: (item) {
-              setState(() {
-                // Remove the image from its current location
-                for (var c in customers) {
-                  c.items.remove(item);
-                }
-                if (images.contains(item)) {
-                  images.remove(item);
-                }
-                
-                // Add to the new customer
-                customer.items.add(item);
-              });
-            },
-          ),
-        );
-      }).toList(),
-    ),
-  );
-}
+    return Container(
+      height: MediaQuery.of(context).size.height * 0.6, // Adjust the height as needed
+      child: Column(
+        children: customers.map((customer) {
+          return Expanded(
+            child: CustomerCart(
+              customer: customer,
+              highlighted: false,
+              crossOutMode: crossOutMode, // Pass crossOutMode to CustomerCart
+              onImageDropped: (item) {
+                setState(() {
+                  // Remove the image from its current location
+                  for (var c in customers) {
+                    c.items.remove(item);
+                  }
+                  if (images.contains(item)) {
+                    images.remove(item);
+                  }
+                  
+                  // Add to the new customer
+                  customer.items.add(item);
+                });
+              },
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
 
   Widget buildImage(ImageData imageData) {
-    return LongPressDraggable<ImageData>(
-      data: imageData,
-      feedback: Material(
-        child: SizedBox(
-          width: 100,
-          height: 100,
-          child: Image.memory(
-            imageData.bytes,
-            fit: BoxFit.cover,
+    return Stack(
+      children: [
+        GestureDetector(
+          onTap: () {
+            if (crossOutMode) {
+              _toggleCrossOut(imageData);
+            } else {
+              _viewImage(imageData);
+            }
+          },
+          child: SizedBox(
+            width: 100,
+            height: 100,
+            child: Image.memory(
+              imageData.bytes,
+              fit: BoxFit.cover,
+            ),
           ),
         ),
-      ),
-      childWhenDragging: Container(), // Display an empty container when dragging
-      child: GestureDetector(
-        onTap: () => _viewImage(imageData),
-        child: SizedBox(
-          width: 100,
-          height: 100,
-          child: Image.memory(
-            imageData.bytes,
-            fit: BoxFit.cover,
+        if (imageData.crossedOut)
+          Positioned.fill(
+            child: Image.asset(
+              'assets/red_cross.png', // Use a different predetermined image
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
-      ),
+      ],
     );
   }
 
@@ -160,34 +170,42 @@ class TierListPageState extends State<TierListPage> {
       ),
       itemCount: images.length,
       itemBuilder: (context, index) {
-        return buildImage(images[index]);
+        return DraggableImage(
+          imageData: images[index],
+          crossOutMode: crossOutMode,
+          onDragComplete: (imageData) {
+            setState(() {
+              images.remove(imageData);
+            });
+          },
+          onToggleCrossOut: _toggleCrossOut, // Pass the callback function
+        );
       },
     );
   }
 
-Future<void> pickImages() async {
-  final pickedFiles = await FilePicker.platform.pickFiles(
-    type: FileType.image,
-    allowMultiple: true,
-  );
+  Future<void> pickImages() async {
+    final pickedFiles = await FilePicker.platform.pickFiles(
+      type: FileType.image,
+      allowMultiple: true,
+    );
 
-  if (pickedFiles != null) {
-    setState(() {
-      for (final file in pickedFiles.files) {
-        final bytes = file.bytes ?? Uint8List(0); // Provide a default value if bytes is null
-        final imageData = ImageData(
-          DateTime.now().millisecondsSinceEpoch.toString(),
-          file.name,
-          bytes: bytes,
-          crossedOut: false,
-          isBase64: false,
-        );
-        images.add(imageData);
-        customers[0].items.add(imageData);
-      }
-    });
+    if (pickedFiles != null) {
+      setState(() {
+        for (final file in pickedFiles.files) {
+          final bytes = file.bytes ?? Uint8List(0); // Provide a default value if bytes is null
+          final imageData = ImageData(
+            DateTime.now().millisecondsSinceEpoch.toString(),
+            file.name,
+            bytes: bytes,
+            crossedOut: false,
+            isBase64: false,
+          );
+          images.add(imageData);
+        }
+      });
+    }
   }
-}
 
   Future<void> saveTierList() async {
     final jsonImages = images.map((image) => image.toJson()).toList();
@@ -248,52 +266,93 @@ class ImageData {
 class DraggableImage extends StatelessWidget {
   final ImageData imageData;
   final Function(ImageData) onDragComplete;
+  final bool crossOutMode; // Add crossOutMode field
+  final Function(ImageData) onToggleCrossOut; // Add onToggleCrossOut callback
 
   const DraggableImage({
     super.key,
     required this.imageData,
     required this.onDragComplete,
+    required this.crossOutMode, // Add crossOutMode parameter
+    required this.onToggleCrossOut, // Add onToggleCrossOut parameter
   });
 
   @override
   Widget build(BuildContext context) {
-    return Draggable<ImageData>(
-      data: imageData,
-      feedback: Container(
-        width: 100,
-        height: 100,
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: MemoryImage(imageData.bytes),
-            fit: BoxFit.cover,
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.black.withOpacity(0.5),
-              blurRadius: 5,
-              spreadRadius: 1,
+    return Stack(
+      children: [
+        if (!crossOutMode)
+          LongPressDraggable<ImageData>(
+            data: imageData,
+            feedback: Container(
+              width: 100,
+              height: 100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: MemoryImage(imageData.bytes),
+                  fit: BoxFit.cover,
+                ),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.5),
+                    blurRadius: 5,
+                    spreadRadius: 1,
+                  ),
+                ],
+              ),
             ),
-          ],
-        ),
-      ),
-      childWhenDragging: Container(
-        width: 100,
-        height: 100,
-        color: Colors.grey.withOpacity(0.5),
-      ),
-      child: Container(
-        width: 100,
-        height: 100,
-        margin: const EdgeInsets.all(4),
-        decoration: BoxDecoration(
-          image: DecorationImage(
-            image: MemoryImage(imageData.bytes),
-            fit: BoxFit.cover,
+            childWhenDragging: Container(
+              width: 100,
+              height: 100,
+              color: Colors.grey.withOpacity(0.5),
+            ),
+            child: Container(
+              width: 100,
+              height: 100,
+              margin: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: MemoryImage(imageData.bytes),
+                  fit: BoxFit.cover,
+                ),
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+            ),
+            onDragCompleted: () => onDragComplete(imageData),
+          )
+        else
+          GestureDetector(
+            onTap: () {
+              onToggleCrossOut(imageData);
+            },
+            child: Container(
+              width: 100,
+              height: 100,
+              margin: const EdgeInsets.all(4),
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: MemoryImage(imageData.bytes),
+                  fit: BoxFit.cover,
+                ),
+                border: Border.all(color: Colors.white, width: 2),
+              ),
+            ),
           ),
-          border: Border.all(color: Colors.white, width: 2),
-        ),
-      ),
-      onDragCompleted: () => onDragComplete(imageData),
+        if (imageData.crossedOut) // Always show the overlay if crossedOut is true
+          Positioned.fill(
+            child: IgnorePointer( // Ensure the overlay does not interfere with drag-and-drop
+              child: Container(
+                color: Colors.red.withOpacity(0.5),
+                child: Center(
+                  child: Image.asset(
+                    'assets/red_cross.png', // Use a different predetermined image
+                    fit: BoxFit.cover,
+                  ),
+                ),
+              ),
+            ),
+          ),
+      ],
     );
   }
 }
@@ -303,11 +362,13 @@ class CustomerCart extends StatelessWidget {
     super.key,
     required this.customer,
     this.highlighted = false,
+    required this.crossOutMode, // Add crossOutMode parameter
     required this.onImageDropped,
   });
 
   final Customer customer;
   final bool highlighted;
+  final bool crossOutMode; // Add crossOutMode field
   final Function(ImageData) onImageDropped;
 
   @override
@@ -318,45 +379,79 @@ class CustomerCart extends StatelessWidget {
         border: Border.all(color: customer.color, width: 2),
       ),
       height: 150, // Adjust height as needed
-      child: DragTarget<ImageData>(
-        builder: (context, candidateData, rejectedData) {
-          return Column(
-            children: [
-              Container(
-                padding: const EdgeInsets.all(8),
-                color: customer.color,
-                child: Text(
-                  customer.name,
-                  style: const TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
+      child: crossOutMode
+          ? Row(
+              children: [
+                Container(
+                  width: 100, // Fixed width for the title
+                  padding: const EdgeInsets.all(8),
+                  color: customer.color,
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    customer.name,
+                    style: const TextStyle(
+                      color: Colors.white,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
                 ),
-              ),
-              Expanded(
-                child: ListView(
-                  scrollDirection: Axis.horizontal,
-                  children: [
-                    ...customer.items.map((item) => DraggableImage(
-                          imageData: item,
-                          onDragComplete: (imageData) {
-                            // Handle drag completion if needed
-                          },
-                        )),
-                  ],
+                Expanded(
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: [
+                      ...customer.items.map((item) {
+                        return buildImage(item);
+                      }),
+                    ],
+                  ),
                 ),
-              ),
-            ],
-          );
-        },
-        onAccept: (imageData) {
-          onImageDropped(imageData);
-        },
-      ),
+              ],
+            )
+          : DragTarget<ImageData>(
+              builder: (context, candidateData, rejectedData) {
+                return Row(
+                  children: [
+                    Container(
+                      width: 100, // Fixed width for the title
+                      padding: const EdgeInsets.all(8),
+                      color: customer.color,
+                      alignment: Alignment.centerLeft,
+                      child: Text(
+                        customer.name,
+                        style: const TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    Expanded(
+                      child: ListView(
+                        scrollDirection: Axis.horizontal,
+                        children: [
+                          ...customer.items.map((item) {
+                            return DraggableImage(
+                              imageData: item,
+                              crossOutMode: crossOutMode, // Pass crossOutMode to DraggableImage
+                              onDragComplete: (imageData) {
+                                // Handle drag completion if needed
+                              },
+                              onToggleCrossOut: (imageData) {}, // No-op for CustomerCart
+                            );
+                          }),
+                        ],
+                      ),
+                    ),
+                  ],
+                );
+              },
+              onAccept: (imageData) {
+                if (!crossOutMode) {
+                  onImageDropped(imageData);
+                }
+              },
+            ),
     );
   }
-
-
 
   Widget buildImage(ImageData imageData) {
     return Padding(
